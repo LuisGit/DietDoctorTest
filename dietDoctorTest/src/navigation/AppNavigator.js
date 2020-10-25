ed3694ee86ddb92719cde4c39545bd8686998189
Luis Alvarado Solano <luis.alvarado@gorillalogic.com>
2020-10-24 23:58:31 -0600

    Adding Log out  option to the Tab Menu

--- dietDoctorTest/src/navigation/AppNavigator.js
+++ dietDoctorTest/src/navigation/AppNavigator.js
@@ -1,16 +1,65 @@
 import React from 'react';
+import { Alert } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import RecipiesScreen from '../screens/RecipiesScreen';
 import MealsScreen from '../screens/MealsScreen';
 
+const LogOutScreen = () => {};
+
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Recipies') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Meals') {
+            iconName = focused ? 'albums' : 'albums-outline';
+          } else if (route.name === 'Logout') {
+            iconName = focused ? 'rocket' : 'rocket-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
       <Tab.Screen name="Recipies" component={RecipiesScreen} />
       <Tab.Screen name="Meals" component={MealsScreen} />
+      <Tab.Screen
+        name="Logout"
+        component={LogOutScreen}
+        listeners={({ navigation, route }) => ({
+          tabPress: (e) => {
+            e.preventDefault();
+            return Alert.alert(
+              // Shows up the alert without redirecting anywhere
+              'Confirmation required',
+              'Do you really want to logout?',
+              [
+                {
+                  text: 'Accept',
+                  onPress: () => {
+                    navigation.navigate('LandingScreen');
+                  },
+                },
+                { text: 'Cancel' },
+              ],
+            );
+          },
+        })}
+      />
     </Tab.Navigator>
   );
 };
